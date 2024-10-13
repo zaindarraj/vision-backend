c9e082452e98145a9245a64dd646b759e7c3599f
zaindarraj <zaindarraj@gmail.com>
2024-10-13 13:12:56 +0300

    Fixes

--- app/middleware/guest_middleware.ts
+++ app/middleware/guest_middleware.ts
@@ -1,4 +1,3 @@
-import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 
 export default class GuestMiddleWare {
@@ -6,7 +5,6 @@ export default class GuestMiddleWare {
    * The URL to redirect to, when authentication fails
    */
   async handle(
-    ctx: HttpContext,
     next: NextFn,
   ) {
     return await next();
